9bc244b4300acdda86ee4dfe0ebea3f0286f8806
SnakyMz <Soban.syeed@outlook.com>
2023-12-06 20:22:33 +0530

    Add form for NewCar

--- src/components/pages/NewCar.jsx
+++ src/components/pages/NewCar.jsx
@@ -1,7 +1,65 @@
 function NewCar() {
   return (
     <section className="newCar">
-      <h1>New Car</h1>
+      <h2>New Car</h2>
+
+      <form onSubmit={}>
+        <fieldset>
+          <label htmlFor="text">Model Name:</label>
+          <input
+            type="text"
+            name="model"
+            className="textFields"
+            value={}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="number">Number of Seats:</label>
+          <input
+            type="number"
+            name="seats"
+            className="textFields"
+            value={}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="text">Car Description:</label>
+          <input
+            type="text"
+            name="description"
+            className="textFields"
+            value={}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="text">Price per day:</label>
+          <input
+            type="text"
+            name="price_per_day"
+            className="textFields"
+            value={}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="text">Vehicle Type:</label>
+          <input
+            type="text"
+            name="vehicle_type"
+            className="textFields"
+            value={}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="text">Vehicle Image:</label>
+          <input
+            type="text"
+            name="image_url"
+            className="textFields"
+            value={}
+          />
+        </fieldset>
+        <button type="submit">Add New Car</button>
+      </form>
     </section>
   );
 }
