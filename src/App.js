dbff5af6cb94b736db503895b96158adbdfbf7a4
Rebelzob <karimdavid07@gmail.com>
2023-12-06 20:17:53 +0000

    Add navigation component to main layout

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import RentalNew from './components/pages/RentalNew';
 import SignUp from './components/pages/SignUp';
 import HomePage from './components/pages/HomePage';
 import ProtectedRoute from './components/ProtectedRoute';
+import Navigation from './components/shared/Navigation';
 
 const App = () => {
   const active = useSelector((state) => state.credentials.active);
@@ -20,72 +21,75 @@ const App = () => {
       <header>
         <NotificationContainer />
       </header>
-      <Routes>
-        <Route
-          path="/login"
-          element={(
-            <ProtectedRoute isAuthenticated={!active} redirectTo="/">
-              <Login />
-            </ProtectedRoute>
-          )}
-        />
-        <Route
-          path="/signup"
-          element={(
-            <ProtectedRoute isAuthenticated={!active} redirectTo="/">
-              <SignUp />
-            </ProtectedRoute>
-          )}
-        />
-        <Route
-          path="/"
-          element={(
-            <ProtectedRoute isAuthenticated={active} redirectTo="/login">
-              <HomePage />
-            </ProtectedRoute>
-          )}
-        />
-        <Route
-          path="/newcar"
-          element={(
-            <ProtectedRoute isAuthenticated={active} redirectTo="/login">
-              <NewCar />
-            </ProtectedRoute>
-          )}
-        />
-        <Route
-          path="/deletecar"
-          element={(
-            <ProtectedRoute isAuthenticated={active} redirectTo="/login">
-              <CarDelete />
-            </ProtectedRoute>
-          )}
-        />
-        <Route
-          path="/car/:id"
-          element={(
-            <ProtectedRoute isAuthenticated={active} redirectTo="/login">
-              <CarDetail />
-            </ProtectedRoute>
-          )}
-        />
-        <Route
-          path="/rentals"
-          element={(
-            <ProtectedRoute isAuthenticated={active} redirectTo="/login">
-              <Rentals />
-            </ProtectedRoute>
-          )}
-        />
-        <Route
-          path="/rentals/new"
-          element={(
-            <ProtectedRoute isAuthenticated={active} redirectTo="/login">
-              <RentalNew />
-            </ProtectedRoute>
-          )}
-        />
-      </Routes>
+      <main className="flex">
+        {active && <Navigation />}
+        <Routes>
+          <Route
+            path="/login"
+            element={(
+              <ProtectedRoute isAuthenticated={!active} redirectTo="/">
+                <Login />
+              </ProtectedRoute>
+            )}
+          />
+          <Route
+            path="/signup"
+            element={(
+              <ProtectedRoute isAuthenticated={!active} redirectTo="/">
+                <SignUp />
+              </ProtectedRoute>
+            )}
+          />
+          <Route
+            path="/"
+            element={(
+              <ProtectedRoute isAuthenticated={active} redirectTo="/login">
+                <HomePage />
+              </ProtectedRoute>
+            )}
+          />
+          <Route
+            path="/newcar"
+            element={(
+              <ProtectedRoute isAuthenticated={active} redirectTo="/login">
+                <NewCar />
+              </ProtectedRoute>
+            )}
+          />
+          <Route
+            path="/deletecar"
+            element={(
+              <ProtectedRoute isAuthenticated={active} redirectTo="/login">
+                <CarDelete />
+              </ProtectedRoute>
+            )}
+          />
+          <Route
+            path="/car/:id"
+            element={(
+              <ProtectedRoute isAuthenticated={active} redirectTo="/login">
+                <CarDetail />
+              </ProtectedRoute>
+            )}
+          />
+          <Route
+            path="/rentals"
+            element={(
+              <ProtectedRoute isAuthenticated={active} redirectTo="/login">
+                <Rentals />
+              </ProtectedRoute>
+            )}
+          />
+          <Route
+            path="/rentals/new"
+            element={(
+              <ProtectedRoute isAuthenticated={active} redirectTo="/login">
+                <RentalNew />
+              </ProtectedRoute>
+            )}
+          />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
