2d598b6f332699447594b927ed96344d05b2eb54
Rysth <johnpalacios.t@gmail.com>
2023-12-06 14:29:44 -0500

    Start HomePage component.

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <section className="h-screen ">
+      <article className="flex flex-col items-center justify-center h-full max-w-screen-xl mx-auto">
+        <header className="py-2 text-center">
+          <h2 className="text-xl font-black tracking-wider uppercase sm:text-2xl md:text-4xl">
+            Lastest Models
+          </h2>
+          <h3 className="py-1 text-sm font-semibold text-gray-400 sm:text-base">
+            Please select a Car Model
+          </h3>
+        </header>
+      </article>
+    </section>
   );
 }
 
