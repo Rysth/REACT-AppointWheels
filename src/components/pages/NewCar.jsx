d463fc60bed2fe156583681a5720f772816ef559
Rysth <johnpalacios.t@gmail.com>
2023-12-06 10:34:57 -0500

    Improve styles and test functionality.

--- src/components/pages/NewCar.jsx
+++ src/components/pages/NewCar.jsx
@@ -1,76 +1,113 @@
-import { useDispatch } from "react-redux";
-import { addCar } from "../../redux/slices/carsSlice";
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { addCar } from '../../redux/slices/carsSlice';
 
 function NewCar() {
   const dispatch = useDispatch();
+  const navigator = useNavigate();
   const { register, handleSubmit } = useForm();
 
   const submitAddCar = (data) => {
-    dispatch(addCar(data));
-  }
-  
-  return (
-    <section className="newCar">
-      <h2>New Car</h2>
+    dispatch(addCar(data)).then(() => navigator('/'));
+  };
 
-      <form onSubmit={handleSubmit(submitAddCar)}>
-        <fieldset>
-          <label htmlFor="text">Model Name:</label>
-          <input
-            type="text"
-            {...register('model')}
-            className="textFields"
-            required
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="text">Number of Seats:</label>
-          <input
-            type="text"
-            {...register('seats')}
-            className="textFields"
-            required
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="text">Car Description:</label>
-          <input
-            type="text"
-            {...register('description')}
-            className="textFields"
-            required
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="text">Price per day:</label>
-          <input
-            type="text"
-            {...register('price_per_day')}
-            className="textFields"
-            required
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="text">Vehicle Type:</label>
-          <input
-            type="text"
-            {...register('vehicle_type')}
-            className="textFields"
-            required
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="text">Vehicle Image:</label>
-          <input
-            type="text"
-            {...register('image_url')}
-            className="textFields"
-            required
-          />
-        </fieldset>
-        <button type="submit">Add New Car</button>
-      </form>
+  return (
+    <section className="bg-white">
+      <article className="max-w-screen-xl mx-auto">
+        <header className="mt-2 text-center">
+          <h2 className="text-3xl font-bold sm:text-4xl md:text-5xl">
+            New Car
+          </h2>
+        </header>
+        {/* eslint-disable */}
+        <form onSubmit={handleSubmit(submitAddCar)}>
+          <fieldset>
+            <label
+              htmlFor="model"
+              className="relative grid gap-2 font-semibold"
+            >
+              Model Name:
+              <input
+                type="text"
+                {...register('model')}
+                className="flex-1 p-2 font-normal border rounded-lg shadow-lg focus:outline-none focus:border-gray-500"
+                required
+              />
+            </label>
+          </fieldset>
+          <fieldset>
+            <label
+              htmlFor="seats"
+              className="relative grid gap-2 font-semibold"
+            >
+              Number of Seats:
+              <input
+                type="number"
+                {...register('seats')}
+                className="flex-1 p-2 font-normal border rounded-lg shadow-lg focus:outline-none focus:border-gray-500"
+                required
+              />
+            </label>
+          </fieldset>
+          <fieldset>
+            <label
+              htmlFor="description"
+              className="relative grid gap-2 font-semibold"
+            >
+              Car Description:
+              <input
+                type="text"
+                {...register('description')}
+                className="flex-1 p-2 font-normal border rounded-lg shadow-lg focus:outline-none focus:border-gray-500"
+                required
+              />
+            </label>
+          </fieldset>
+          <fieldset>
+            <label
+              htmlFor="price_per_day"
+              className="relative grid gap-2 font-semibold"
+            >
+              Price per day:
+              <input
+                type="number"
+                {...register('price_per_day')}
+                className="flex-1 p-2 font-normal border rounded-lg shadow-lg focus:outline-none focus:border-gray-500"
+                required
+              />
+            </label>
+          </fieldset>
+          <fieldset>
+            <label
+              htmlFor="vehicle_type"
+              className="relative grid gap-2 font-semibold"
+            >
+              Vehicle Type:
+              <input
+                type="text"
+                {...register('vehicle_type')}
+                className="flex-1 p-2 font-normal border rounded-lg shadow-lg focus:outline-none focus:border-gray-500"
+                required
+              />
+            </label>
+          </fieldset>
+          <fieldset>
+            <label htmlFor="text" className="relative grid gap-2 font-semibold">
+              Vehicle Image:
+              <input
+                type="text"
+                {...register('image_url')}
+                className="flex-1 p-2 font-normal border rounded-lg shadow-lg focus:outline-none focus:border-gray-500"
+                required
+              />
+            </label>
+          </fieldset>
+          <button type="submit" className="btn btn-primary">
+            Add New Car
+          </button>
+        </form>
+      </article>
     </section>
   );
 }
