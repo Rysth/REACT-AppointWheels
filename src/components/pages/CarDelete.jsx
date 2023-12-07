ca6500064ccbe9dd0ced3da40a5840cc3bb97d91
SnakyMz <Soban.syeed@outlook.com>
2023-12-07 20:00:49 +0530

    Fix minor issues

--- src/components/pages/CarDelete.jsx
+++ src/components/pages/CarDelete.jsx
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCars, removeCar } from "../../redux/slices/carsSlice";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCars, removeCar } from '../../redux/slices/carsSlice';
 
 function CarDelete() {
-  const { carsArray, loading } = useSelector((store) => store.carStore);
+  const { carsArray, loading } = useSelector((store) => store.carsStore);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchCars());
   }, [dispatch]);
 
-  if(loading) {
-    return(<div>Loading...</div>)
+  if (loading) {
+    return (<div>Loading...</div>);
   }
 
   return (
@@ -25,7 +25,7 @@ function CarDelete() {
               <h3>{car.model}</h3>
               <p>{car.description}</p>
             </div>
-            <button type="button" className="btn-primary" onClick={() => dispatch(removeCar(car.id))}>Delete</button>
+            <button type="button" className="btn btn-primary" onClick={() => dispatch(removeCar(car.id))}>Delete</button>
           </div>
         ))}
       </div>
