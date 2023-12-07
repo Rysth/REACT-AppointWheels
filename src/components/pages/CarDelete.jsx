8dc808174caba727feb2b1a4dae4ba5dc3b6dcd2
SnakyMz <Soban.syeed@outlook.com>
2023-12-07 18:51:46 +0530

    Add CarDelete page

--- src/components/pages/CarDelete.jsx
+++ src/components/pages/CarDelete.jsx
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCars, removeCar } from "../../redux/slices/carsSlice";
+
 function CarDelete() {
+  const { carsArray, loading } = useSelector((store) => store.carStore);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCars());
+  }, [dispatch]);
+
+  if(loading) {
+    return(<div>Loading...</div>)
+  }
+
   return (
     <section className="carDelete">
-      <h1>Car Delete</h1>
+      <h1>Deleting Car</h1>
+      <div className="carDeleteList">
+        {carsArray.map((car) => (
+          <div className="car" key={car.id}>
+            <img src={car.image_url} alt={car.model} className="carImage" />
+            <div className="carDeleteInfo">
+              <h3>{car.model}</h3>
+              <p>{car.description}</p>
+            </div>
+            <button type="button" className="btn-primary" onClick={() => dispatch(removeCar(car.id))}>Delete</button>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
