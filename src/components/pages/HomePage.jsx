f4f7e3a443eaaac35b61db4aabe927bfca3d05b5
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-12-07 12:15:35 -0300

    change homepage to use the store and create links

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,35 +1,13 @@
 import { useEffect, useState } from 'react';
 import { LuArrowBigLeftDash, LuArrowBigRightDash } from 'react-icons/lu';
-
-const carArrays = [
-  {
-    id: 1,
-    model: 'Corolla',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla delectus et perspiciatis pariatur dolore, expedita eaque omnis maxime. Tempore repellendus error, assumenda laboriosam accusantium fugiat exercitationem quasi itaque fugit modi!',
-    image_url:
-      'https://www.motortrend.com/uploads/sites/10/2019/03/2020-toyota-corolla-le-sedan-angular-front.png',
-  },
-  {
-    id: 2,
-    model: 'Camry',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla delectus et perspiciatis pariatur dolore, expedita eaque omnis maxime. Tempore repellendus error, assumenda laboriosam accusantium fugiat exercitationem quasi itaque fugit modi!',
-    image_url: 'https://jzheng100823.github.io/hicproj/images/ToyotaCamry.png ',
-  },
-  {
-    id: 3,
-    model: 'Land Cruiser',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla delectus et perspiciatis pariatur dolore, expedita eaque omnis maxime. Tempore repellendus error, assumenda laboriosam accusantium fugiat exercitationem quasi itaque fugit modi!',
-    image_url:
-      'https://www.motortrend.com/uploads/sites/10/2016/05/2016-toyota-landcruiser-4wd-suv-angular-front.png',
-  },
-];
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchCars } from '../../redux/slices/carsSlice';
 
 function HomePage() {
+  const { carsArray, loading } = useSelector((store) => (store.carsStore));
   const [index, setIndex] = useState(0);
-  const [carsQuantity, setCarsQuantity] = useState(carArrays.length);
+  const [carsQuantity, setCarsQuantity] = useState(carsArray.length);
 
   const increaseIndex = () => {
     if (index < carsQuantity - 1) setIndex(index + 1);
@@ -40,8 +18,17 @@ function HomePage() {
   };
 
   useEffect(() => {
-    setCarsQuantity(carArrays.length);
+    setCarsQuantity(carsArray.length);
   }, []);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (loading) {
+      dispatch(fetchCars());
+    }
+  }, [dispatch, loading]);
+
+  if (loading) { return (<div>loading</div>); }
 
   return (
     <section className="h-screen ">
@@ -69,30 +56,32 @@ function HomePage() {
             <LuArrowBigLeftDash className="text-xl sm:text-3xl" />
           </button>
           <ul className="grid flex-1 gap-2 md:grid-cols-3 sm:gap-10">
-            {carArrays.slice(index, 3 + index).map((car) => (
-              <li key={car.id}>
-                <div className="flex flex-col items-center justify-center w-full h-full gap-5">
-                  <picture className="relative grid overflow-visible sm:mb-10 place-items-center sm:px-5">
-                    <span className="absolute z-20 w-32 h-32 rounded-full bg-slate-200 md:w-36 md:h-36 xl:w-60 xl:h-60" />
-                    <img
-                      src={car.image_url}
-                      alt=""
-                      className="z-50 object-contain w-full h-full rounded-full "
-                    />
-                  </picture>
-                  <div className="px-10 text-center">
-                    <h3 className="font-bold uppercase sm:text-md ">
-                      {car.model}
-                    </h3>
-                    <p className="mb-3 text-xl tracking-wider text-gray-200 sm:text-2xl ">
-                      .........................
-                    </p>
-                    <p className="overflow-hidden text-xs text-center text-gray-400 sm:text-sm max-h-14">
-                      {car.description}
-                    </p>
+            {carsArray.slice(index, 3 + index).map((car) => (
+              <Link to={`/${car.id}`} key={car}>
+                <li key={car.id}>
+                  <div className="flex flex-col items-center justify-center w-full h-full gap-5">
+                    <picture className="relative grid overflow-visible sm:mb-10 place-items-center sm:px-5">
+                      <span className="absolute z-20 w-32 h-32 rounded-full bg-slate-200 md:w-36 md:h-36 xl:w-60 xl:h-60" />
+                      <img
+                        src={car.image_url}
+                        alt=""
+                        className="z-50 object-contain w-full h-full rounded-full "
+                      />
+                    </picture>
+                    <div className="px-10 text-center">
+                      <h3 className="font-bold uppercase sm:text-md ">
+                        {car.model}
+                      </h3>
+                      <p className="mb-3 text-xl tracking-wider text-gray-200 sm:text-2xl ">
+                        .........................
+                      </p>
+                      <p className="overflow-hidden text-xs text-center text-gray-400 sm:text-sm max-h-14">
+                        {car.description}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </li>
+                </li>
+              </Link>
             ))}
           </ul>
           <button
