16a0f5b307ae760aebc3d4bbba4d138ce65774d8
Rebelzob <karimdavid07@gmail.com>
2023-12-08 16:17:13 +0000

    Update rental API endpoints

--- src/redux/slices/rentalsSlice.js
+++ src/redux/slices/rentalsSlice.js
@@ -2,27 +2,31 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const authTokenData = sessionStorage.getItem('authToken');
+const user = JSON.parse(sessionStorage.getItem('userCredentials'));
 
 // Async thunk to fetch rentals
 export const fetchRentals = createAsyncThunk('rentals/fetchRentals', async () => {
-  const response = await axios.get('/api/v1/rentals', {
+  const response = await axios.get(`http://localhost:3001/api/v1/users/${user.id}/rentals`, {
     headers: { Authorization: authTokenData },
+    withCredentials: true,
   });
   return response.data;
 });
 
 // Async thunk to create a rental
 export const createRental = createAsyncThunk('rentals/createRental', async (rental) => {
-  const response = await axios.post('/api/v1/rentals', rental, {
+  const response = await axios.post(`http://localhost:3001/api/v1/users/${user.id}/rentals`, rental, {
     headers: { Authorization: authTokenData },
+    withCredentials: true,
   });
   return response.data;
 });
 
 // Async thunk to delete a rental
 export const deleteRental = createAsyncThunk('rentals/deleteRental', async (id) => {
-  await axios.delete(`/api/v1/rentals/${id}`, {
+  await axios.delete(`http://localhost:3001/api/v1/users/${user.id}/rentals/${id}`, {
     headers: { Authorization: authTokenData },
+    withCredentials: true,
   });
   return id;
 });
