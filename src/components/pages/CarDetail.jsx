eb2049eef7009f1f691237228e5819b923ffcae7
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-12-07 21:17:59 -0300

    add data picker styles - add data calculate functions

--- src/components/pages/CarDetail.jsx
+++ src/components/pages/CarDetail.jsx
@@ -1,20 +1,53 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import ReactDatePicker from 'react-datepicker';
 import { fetchCars } from '../../redux/slices/carsSlice';
 
 function CarDetail() {
+  const dispatch = useDispatch();
   const { id } = useParams();
   const carId = parseInt(id, 10);
   const { carsArray, loading } = useSelector((store) => (store.carsStore));
   const car = carsArray.find((el) => el.id === carId);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const [totalDays, setTotalDays] = useState();
+  const [amount, setAmount] = useState();
 
-  const dispatch = useDispatch();
   useEffect(() => {
     if (loading) {
       dispatch(fetchCars());
     }
   }, [dispatch, loading]);
+
+  const calculateTotalDays = () => {
+    if (startDate && endDate) {
+      const totalMilliseconds = endDate.getTime() - startDate.getTime();
+      const days = (totalMilliseconds / (1000 * 60 * 60 * 24));
+      setTotalDays(Math.round(days));
+    }
+  };
+
+  const calculateTotalAmout = () => {
+    if (car) {
+      setAmount(totalDays * car.price_per_day + 200);
+    }
+  };
+
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+
+  useEffect(() => {
+    calculateTotalDays();
+  }, [startDate, endDate]);
+
+  useEffect(() => {
+    calculateTotalAmout();
+  }, [calculateTotalDays]);
 
   if (loading) { return (<div>loading</div>); }
 
@@ -29,7 +62,7 @@ function CarDetail() {
             className="z-50 object-contain w-full h-full rounded-full "
           />
         </picture>
-        <div className="flex flex-col items-end justify-center w-full h-full mr-10 border">
+        <div className="flex flex-col items-end justify-center w-full h-full mr-10">
           <h2 className="pb-8 text-xl font-bold uppercase sm:text-md">{car.model}</h2>
           <div className="flex flex-row justify-between w-full p-2 bg-slate-300">
             <p className="text-xs text-center sm:text-sm max-h-14 whitespace-nowrap">Price Per Day</p>
@@ -44,11 +77,24 @@ function CarDetail() {
           </div>
           <div className="flex flex-row justify-between w-full p-2 bg-slate-300">
             <p className="pr-4 text-xs text-center sm:text-sm max-h-14 whitespace-nowrap">Total Amount Payable</p>
-            <p className="text-xs text-center sm:text-sm max-h-14">$9000 </p>
+            <p className="text-xs text-center sm:text-sm max-h-14">
+              $
+              {amount || 0}
+            </p>
           </div>
           <div className="flex flex-row justify-between w-full p-2">
             <p className="pr-4 text-xs text-center sm:text-sm max-h-14">Duration</p>
-            <p className="text-xs text-center sm:text-sm max-h-14">48 Months</p>
+            <p className="text-xs text-center sm:text-sm max-h-14">{totalDays}</p>
+          </div>
+          <div className="flex flex-row justify-between w-full p-2">
+            <ReactDatePicker
+              selected={startDate}
+              onChange={onChange}
+              startDate={startDate}
+              endDate={endDate}
+              selectsRange
+              inline
+            />
           </div>
         </div>
       </article>
