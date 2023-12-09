73b5cf1480fbf515f07f3fe875a63d7102cc71bd
Rysth <johnpalacios.t@gmail.com>
2023-12-09 11:16:17 -0500

    Improve Sidebar and Home desktop design.

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { fetchCars } from '../../redux/slices/carsSlice';
 
 function HomePage() {
-  const { carsArray, loading } = useSelector((store) => (store.carsStore));
+  const { carsArray, loading } = useSelector((store) => store.carsStore);
   const [index, setIndex] = useState(0);
   const [carsQuantity, setCarsQuantity] = useState(carsArray.length);
 
@@ -28,7 +28,9 @@ function HomePage() {
     }
   }, [dispatch, loading]);
 
-  if (loading) { return (<div>loading</div>); }
+  if (loading) {
+    return <div>loading</div>;
+  }
 
   return (
     <section className="h-screen ">
@@ -58,20 +60,14 @@ function HomePage() {
           <ul className="grid flex-1 gap-2 md:grid-cols-3 sm:gap-10">
             {carsArray.slice(index, 3 + index).map((car) => (
               <Link to={`/car/${car.id}`} key={car.id}>
-                <li key={car.id}>
+                <li key={car.id} className="h-full">
                   <div className="flex flex-col items-center justify-center w-full h-full gap-5">
                     <picture className="relative grid overflow-visible sm:mb-10 place-items-center sm:px-5">
                       <span className="absolute z-20 w-32 h-32 rounded-full bg-slate-200 md:w-36 md:h-36 xl:w-60 xl:h-60" />
-                      <img
-                        src={car.image_url}
-                        alt=""
-                        className="z-50 object-contain w-full h-full rounded-full "
-                      />
+                      <img src={car.image_url} alt="" className="z-50 object-contain h-[15rem]" />
                     </picture>
                     <div className="px-10 text-center">
-                      <h3 className="font-bold uppercase sm:text-md ">
-                        {car.model}
-                      </h3>
+                      <h3 className="font-bold uppercase sm:text-md ">{car.model}</h3>
                       <p className="mb-3 text-xl tracking-wider text-gray-200 sm:text-2xl ">
                         .........................
                       </p>
--- src/components/shared/Navigation.jsx
+++ src/components/shared/Navigation.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+/* prettier-ignore */
 import {
   FaGithub,
   FaLinkedin,
@@ -14,80 +15,109 @@ function Navigation() {
   const toggleOpenHome = () => setIsOpenHome(!isOpenHome);
 
   return (
-    <nav className="flex flex-col h-screen px-4 py-8 bg-white border-r-2 w-60 text-back">
-      <img src={AppointWheelsLogo} alt="AppointWheels Logo" />
-      <ul className="mt-32 font-semibold uppercase">
-        <li className="mb-2">
-          <NavLink to="/" activeClassName="bg-[var(--green)]" className="block py-1 hover:bg-[var(--green)] hover:text-white text-xl pl-4" onClick={toggleOpenHome}>
+    <nav className="flex flex-col h-screen py-8 bg-white shadow-xl min-w-[13rem] max-w-[13rem] text-back">
+      <picture>
+        <img
+          src={AppointWheelsLogo}
+          alt="AppointWheels Logo"
+          className="block bg-cyan-200"
+        />
+      </picture>
+      <ul className="flex flex-col flex-1 pl-4 font-semibold uppercase mt-14">
+        <li>
+          <NavLink
+            to="/"
+            activeClassName="bg-[var(--green)]"
+            className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition "
+            onClick={toggleOpenHome}
+          >
             Home
           </NavLink>
           {isOpenHome && (
             <ul className="bg-slate-100">
               <li className="mb-1">
-                <NavLink to="/newcar" activeClassName="bg-[var(--green)]" className="block py-1 hover:bg-[var(--green)] hover:text-white pl-4">
+                <NavLink
+                  to="/newcar"
+                  activeClassName="bg-[var(--green)]"
+                  className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white"
+                >
                   Create Car
                 </NavLink>
               </li>
               <li className="mb-1">
-                <NavLink to="/deletecar" activeClassName="bg-[var(--green)]" className="block py-1 hover:bg-[var(--green)] hover:text-white pl-4">
+                <NavLink
+                  to="/deletecar"
+                  activeClassName="bg-[var(--green)]"
+                  className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white"
+                >
                   Delete Car
                 </NavLink>
               </li>
             </ul>
           )}
         </li>
-        <li className="mb-2">
-          <NavLink to="/rentals" activeClassName="bg-[var(--green)]" className="block py-1 hover:bg-[var(--green)] hover:text-white text-xl pl-4">
+        <li>
+          <NavLink
+            to="/rentals"
+            activeClassName="bg-[var(--green)]"
+            className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition"
+          >
             My Rentals
           </NavLink>
         </li>
-        <li className="mb-2">
-          <NavLink to="/rental/new" activeClassName="bg-[var(--green)]" className="block py-1 hover:bg-[var(--green)] hover:text-white text-xl pl-4">
+        <li>
+          <NavLink
+            to="/rental/new"
+            activeClassName="bg-[var(--green)]"
+            className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition"
+          >
             New Rental
           </NavLink>
         </li>
       </ul>
-      <div className="flex justify-center mt-auto">
-        <a
-          href="https://github.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mx-2 hover:text-green-500"
-          aria-label="GitHub"
-        >
-          <FaGithub size={20} />
-        </a>
-        <a
-          href="https://linkedin.com/in/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mx-2 hover:text-green-500"
-          aria-label="LinkedIn"
-        >
-          <FaLinkedin size={20} />
-        </a>
-        <a
-          href="https://twitter.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mx-2 hover:text-green-500"
-          aria-label="Twitter"
-        >
-          <FaTwitter size={20} />
-        </a>
-        <a
-          href="https://angel.co/u/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mx-2 hover:text-green-500"
-          aria-label="AngelList"
-        >
-          <FaAngellist size={20} />
-        </a>
-      </div>
-      <div className="mt-4 text-sm text-center text-gray-500">
-        &copy; 2023 AppointWheels
-      </div>
+      <footer>
+        <div className="flex justify-center mt-auto">
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2 hover:text-green-500"
+            aria-label="GitHub"
+          >
+            <FaGithub size={20} />
+          </a>
+          <a
+            href="https://linkedin.com/in/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2 hover:text-green-500"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedin size={20} />
+          </a>
+          <a
+            href="https://twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2 hover:text-green-500"
+            aria-label="Twitter"
+          >
+            <FaTwitter size={20} />
+          </a>
+          <a
+            href="https://angel.co/u/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2 hover:text-green-500"
+            aria-label="AngelList"
+          >
+            <FaAngellist size={20} />
+          </a>
+        </div>
+        <div className="mt-4 text-sm text-center text-gray-500">
+          &copy; 2023 AppointWheels
+        </div>
+      </footer>
     </nav>
   );
 }
