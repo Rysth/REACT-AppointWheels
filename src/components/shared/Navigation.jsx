b6be2ffe1ab6b785419923a6245833ca3ff8e64c
Rebelzob <karimdavid07@gmail.com>
2023-12-10 18:01:02 +0000

    Update user authentication logic

--- src/components/shared/Navigation.jsx
+++ src/components/shared/Navigation.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 /* prettier-ignore */
+import { FiMenu } from 'react-icons/fi';
 import {
   FaGithub,
   FaLinkedin,
@@ -11,114 +12,177 @@ import AppointWheelsLogo from '../../assets/icons/AppointWheels.png';
 
 function Navigation() {
   const [isOpenHome, setIsOpenHome] = useState(false);
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
 
   const toggleOpenHome = () => setIsOpenHome(!isOpenHome);
+  const toggleOpenMenu = () => setIsOpenMenu(!isOpenMenu);
 
   return (
-    <nav className="flex flex-col h-screen py-8 bg-white shadow-xl min-w-[13rem] max-w-[13rem] text-back">
-      <picture>
-        <img
-          src={AppointWheelsLogo}
-          alt="AppointWheels Logo"
-          className="block bg-cyan-200"
+    <div>
+      <nav className="sm:hidden">
+        <FiMenu
+          onClick={toggleOpenMenu}
+          className="absolute top-0 right-0 z-20 m-4 text-4xl text-black cursor-pointer"
         />
-      </picture>
-      <ul className="flex flex-col flex-1 pl-4 font-semibold uppercase mt-14">
-        <li>
-          <NavLink
-            to="/"
-            activeClassName="bg-[var(--green)]"
-            className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition "
-            onClick={toggleOpenHome}
-          >
-            Home
-          </NavLink>
-          {isOpenHome && (
-            <ul className="bg-slate-100">
-              <li className="mb-1">
-                <NavLink
-                  to="/newcar"
-                  activeClassName="bg-[var(--green)]"
-                  className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white"
-                >
-                  Create Car
-                </NavLink>
-              </li>
-              <li className="mb-1">
-                <NavLink
-                  to="/deletecar"
-                  activeClassName="bg-[var(--green)]"
-                  className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white"
-                >
-                  Delete Car
-                </NavLink>
-              </li>
-            </ul>
-          )}
-        </li>
-        <li>
-          <NavLink
-            to="/rentals"
-            activeClassName="bg-[var(--green)]"
-            className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition"
-          >
-            My Rentals
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/rental/new"
-            activeClassName="bg-[var(--green)]"
-            className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition"
-          >
-            New Rental
-          </NavLink>
-        </li>
-      </ul>
-      <footer>
-        <div className="flex justify-center mt-auto">
-          <a
-            href="https://github.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-2 hover:text-green-500"
-            aria-label="GitHub"
-          >
-            <FaGithub size={20} />
-          </a>
-          <a
-            href="https://linkedin.com/in/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-2 hover:text-green-500"
-            aria-label="LinkedIn"
-          >
-            <FaLinkedin size={20} />
-          </a>
-          <a
-            href="https://twitter.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-2 hover:text-green-500"
-            aria-label="Twitter"
-          >
-            <FaTwitter size={20} />
-          </a>
-          <a
-            href="https://angel.co/u/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-2 hover:text-green-500"
-            aria-label="AngelList"
-          >
-            <FaAngellist size={20} />
-          </a>
+        {isOpenMenu && (
+        <div className="fixed right-0 z-10 w-full py-12 text-black transition-all duration-500 ease-in-out origin-top transform bg-white shadow-xl">
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition "
+                onClick={toggleOpenHome}
+              >
+                Home
+              </NavLink>
+              {isOpenHome && (
+                <ul className="bg-slate-100">
+                  <li className="mb-1">
+                    <NavLink
+                      to="/newcar"
+                      className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white"
+                    >
+                      Create Car
+                    </NavLink>
+                  </li>
+                  <li className="mb-1">
+                    <NavLink
+                      to="/deletecar"
+                      className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white"
+                    >
+                      Delete Car
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li>
+              <NavLink
+                to="/rentals"
+                className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition"
+              >
+                My Rentals
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/rental/new"
+                className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition"
+              >
+                New Rental
+              </NavLink>
+            </li>
+          </ul>
         </div>
-        <div className="mt-4 text-sm text-center text-gray-500">
-          &copy; 2023 AppointWheels
+        )}
+      </nav>
+      <nav className="hidden sm:block">
+        <div className="flex flex-col h-screen py-8 bg-white shadow-xl min-w-[13rem] max-w-[13rem] text-back">
+          <picture>
+            <img
+              src={AppointWheelsLogo}
+              alt="AppointWheels Logo"
+              className="block bg-cyan-200"
+            />
+          </picture>
+          <ul className="flex flex-col flex-1 pl-4 font-semibold uppercase mt-14">
+            <li>
+              <NavLink
+                to="/"
+                activeClassName="bg-[var(--green)]"
+                className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition "
+                onClick={toggleOpenHome}
+              >
+                Home
+              </NavLink>
+              {isOpenHome && (
+                <ul className="bg-slate-100">
+                  <li className="mb-1">
+                    <NavLink
+                      to="/newcar"
+                      activeClassName="bg-[var(--green)]"
+                      className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white"
+                    >
+                      Create Car
+                    </NavLink>
+                  </li>
+                  <li className="mb-1">
+                    <NavLink
+                      to="/deletecar"
+                      activeClassName="bg-[var(--green)]"
+                      className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white"
+                    >
+                      Delete Car
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li>
+              <NavLink
+                to="/rentals"
+                activeClassName="bg-[var(--green)]"
+                className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition"
+              >
+                My Rentals
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/rental/new"
+                activeClassName="bg-[var(--green)]"
+                className="block p-2 pl-4 hover:bg-[var(--green)] hover:text-white text-xl font-bold md:hover:transition"
+              >
+                New Rental
+              </NavLink>
+            </li>
+          </ul>
+          <footer>
+            <div className="flex justify-center mt-auto">
+              <a
+                href="https://github.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mx-2 hover:text-green-500"
+                aria-label="GitHub"
+              >
+                <FaGithub size={20} />
+              </a>
+              <a
+                href="https://linkedin.com/in/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mx-2 hover:text-green-500"
+                aria-label="LinkedIn"
+              >
+                <FaLinkedin size={20} />
+              </a>
+              <a
+                href="https://twitter.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mx-2 hover:text-green-500"
+                aria-label="Twitter"
+              >
+                <FaTwitter size={20} />
+              </a>
+              <a
+                href="https://angel.co/u/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mx-2 hover:text-green-500"
+                aria-label="AngelList"
+              >
+                <FaAngellist size={20} />
+              </a>
+            </div>
+            <div className="mt-4 text-sm text-center text-gray-500">
+              &copy; 2023 AppointWheels
+            </div>
+          </footer>
         </div>
-      </footer>
-    </nav>
+      </nav>
+    </div>
   );
 }
 
